7d6ebabf632f2296ed4f7bcdf8fd5ca30a90fc11
LoveCode <g.mostertpot@gmail.com>
2025-07-11 14:27:29 +0200

    UI: StaffPage
    The bordered initial has been replaced with a clear icon button

--- frontend/src/components/HeaderBar.jsx
+++ frontend/src/components/HeaderBar.jsx
@@ -122,9 +122,7 @@ const HeaderBar = ({ handleDrawerToggle, handleSidebarToggle, isSidebarCollapsed
               aria-haspopup="true"
               aria-expanded={menuOpen ? 'true' : undefined}
             >
-              <Avatar sx={{ width: 32, height: 32, bgcolor: theme.palette.primary.main, color: theme.palette.getContrastText(theme.palette.primary.main), border: `2px solid ${theme.palette.getContrastText(theme.palette.primary.main)}` }}>
-                {currentUser.username ? currentUser.username[0].toUpperCase() : <AccountCircleIcon />}
-              </Avatar>
+              <AccountCircleIcon sx={{ fontSize: 32, color: theme.palette.primary.main }} />
             </IconButton>
             <Menu
               anchorEl={anchorEl}
